2be5d4df4dd2a163618679ffa5e2c12023d76177
Simon Sassi <dualprodu@gmail.com>
2017-01-15 15:07:32 +0100

    changed server

--- ytplayer/js/ytPlayer.js
+++ ytplayer/js/ytPlayer.js
@@ -38,7 +38,7 @@ var currentVolume = 0;
             
             }
         }
-        xmlHttp.open("GET", "http://62.210.243.90/mp3/download.php?v="+id, true); // true for asynchronous 
+        xmlHttp.open("GET", "http://***REMOVED***//mp3/download.php?v="+id, true); // true for asynchronous 
         xmlHttp.send(null);
     };
 
